a67c7a0a18db7d296059992a7aa2d1bb097872c3
csPython <cspython@AD25TF04NGF1J-17.local>
2019-03-26 11:39:30 -0400

    added snow on the bottom and started on snowman

--- winterW.py
+++ winterW.py
@@ -1,8 +1,15 @@
 # Winter Wonderland by Akhi & Pauline
 from graphics import*
 
-Winter = GraphWin("Winter Wonderland",600,600)
-Winter.setCoords(0,0,600,600)
-Winter.setBackground("light blue")
+WinterWin = GraphWin("Winter Wonderland",600,600)
+WinterWin.setCoords(0,0,600,600)
+WinterWin.setBackground("light blue")
 
 
+circle = Circle(Point(100,100), 25)
+circle.setFill("white")
+circle.draw(WinterWin)
+
+Rect = Rectangle(Point(0,0),Point (600,80))
+Rect.setFill("light gray")
+Rect.draw(WinterWin)
