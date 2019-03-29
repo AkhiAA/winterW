9e8007a0f38d9416424fdf38be5290df7fdee629
csPython <cspython@AD25TF04NGF1J-114.local>
2019-03-29 09:53:06 -0400

    added one snowflake

--- winterW.py
+++ winterW.py
@@ -1,15 +1,25 @@
 # Winter Wonderland by Akhi & Pauline
 from graphics import*
 
+
 WinterWin = GraphWin("Winter Wonderland",600,600)
 WinterWin.setCoords(0,0,600,600)
 WinterWin.setBackground("light blue")
+
+## Snowflake
+circle = Circle(Point(550,550), 5)
+circle.setFill("white")
+circle.setOutline("white")
+circle.draw(WinterWin)
+
+
 
 
 circle = Circle(Point(100,100), 25)
 circle.setFill("white")
 circle.draw(WinterWin)
 
+## Gray Snow
 Rect = Rectangle(Point(0,0),Point (600,80))
 Rect.setFill("light gray")
 Rect.draw(WinterWin)
