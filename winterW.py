fbdd899850829f155369c7308c4ec746db2e9425
csPython <cspython@AD25TF04NGF1J-114.local>
2019-04-02 11:16:06 -0400

    made snowflake function in loop

--- winterW.py
+++ winterW.py
@@ -1,19 +1,33 @@
 # Winter Wonderland by Akhi & Pauline
 from graphics import*
+import random
+
+def draw_sf(sX, sY, size, color, WinterWin):
+    sf = Circle(Point(sX, sY), size)
+    sf.setFill(color)
+    sf.setOutline(color)
+    sf.draw(WinterWin)
+
+sX=550
+sY=550
+size=5
+color="white"
 
 WinterWin = GraphWin("Winter Wonderland",600,600)
 WinterWin.setCoords(0,0,600,600)
 WinterWin.setBackground("light blue")
+
+for i in range(50):
+    draw_sf(random.randint(1,600),random.randint(300,600) , size, color, WinterWin)
+
+
 
 ## Snowflake
 circle = Circle(Point(550,550), 5)
 circle.setFill("white")
 circle.setOutline("white")
 circle.draw(WinterWin)
-
-
-
-
+#SnowMan
 circle = Circle(Point(100,100), 25)
 circle.setFill("white")
 circle.draw(WinterWin)
@@ -22,3 +36,8 @@ circle.draw(WinterWin)
 Rect = Rectangle(Point(0,0),Point (600,80))
 Rect.setFill("light gray")
 Rect.draw(WinterWin)
+
+
+
+WinterWin.getMouse()
+WinterWin.close()
