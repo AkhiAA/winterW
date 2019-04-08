4bc96b94a310990ad3269c15fae7ff6599c68633
csPython <cspython@AD25TF04NGF1J-17.local>
2019-04-08 10:25:33 -0400

    improved nose

--- winterW.py
+++ winterW.py
@@ -12,25 +12,31 @@ WinterWin = GraphWin("Winter Wonderland",600,600)
 WinterWin.setCoords(0,0,600,600)
 WinterWin.setBackground("light blue")
 
+anchX = 100
+anchY = 140
+anchR = 28
+
 #bottom cirlce
-draw_circ(100, 100, 34, "white", WinterWin)
+draw_circ(anchX, anchY * .71, anchR * 1.21, "white", WinterWin)
 
 #middle circle
-draw_circ(100, 140, 28, "white", WinterWin)
+draw_circ(anchX, anchY, anchR, "white", WinterWin)
 
 #top circle
-draw_circ(100, 175, 23, "white", WinterWin)
+draw_circ(anchX, anchY * 1.25, anchR * .82, "white", WinterWin)
 
 #snowman eyes
-draw_circ(90, 180, 3, "black", WinterWin)
+draw_circ(anchX - anchR / 3, 180, anchR * .11, "black", WinterWin)
 
 #----------------
-draw_circ(110, 180, 3, "black", WinterWin)
+draw_circ(anchX + anchR / 3, 180, anchR * .11, "black", WinterWin)
 
 #nose
-nose = Line(Point(100,170), Point(120,160))
+nose = Polygon(Point(100,170), Point(105,175), Point(120,160))
+nose.setOutline("orange")
 nose.setFill("orange")
 nose.draw(WinterWin)
+
 #Bottom snow 
 Rect = Rectangle(Point(0,0),Point (600,78))
 Rect.setFill("light gray")
