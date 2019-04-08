c02c5c95af8892ad62e93c5061eed07e8b52372b
csPython <cspython@AD25TF04NGF1J-114.local>
2019-04-08 10:30:11 -0400

    adjusted number of snowflakes

--- winterW.py
+++ winterW.py
@@ -7,7 +7,7 @@ def draw_sf(sX, sY, size, color, sfWin):
     sf.setFill(color)
     sf.setOutline(color)
     sf.draw(WinterWin)
-
+    
 def draw_sfs(flkNum, xStart, xFin, yStart, yFin, size, color,sfsWin):
     for i in range(flkNum):
         draw_sf(random.randint(xStart,xFin),random.randint(yStart,yFin) , size , color ,sfsWin)
@@ -18,7 +18,9 @@ WinterWin.setCoords(0,0,600,600)
 WinterWin.setBackground("light blue")
 
 
-draw_sfs(50, 1, 600, 300, 600, 5, "white", WinterWin) 
+draw_sfs(370, 1, 600, 300, 600, 5, "white", WinterWin) 
+
+
 
 ## Snowflake
 circle = Circle(Point(550,550), 5)
