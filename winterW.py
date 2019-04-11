1b72f0c001e98e081928e44f55369755b9e402ee
csPython <cspython@AD25TF04NGF1J-17.local>
2019-04-11 15:44:33 -0400

    fixed sY in function

--- winterW.py
+++ winterW.py
@@ -2,54 +2,60 @@
 from graphics import*
 import random
 
-def draw_circ(cX, cY, cRad, cColor, cWin):
+def draw_rect(rX, rY, rW, rH, rCol, rWin):   #draws bottom snow
+    Rect = Rectangle(Point(rX,rY),Point (rX + rW, rY + rH))
+    Rect.setFill(rCol)
+    Rect.setOutline(rCol)
+    Rect.draw(rWin)
+
+def draw_circ(cX, cY, cRad, cColor, cWin):  #draws the circle for the snowman 
     circle = Circle(Point(cX,cY), cRad)
     circle.setFill(cColor)
     circle.setOutline(cColor)
-    circle.draw(cWin)  
+    circle.draw(cWin)
+    
 def draw_sm(sX, sY, sRad,sCol,sWin):
-    draw_circ(sX, sY * .71, sRad * 1.21, "white", sWin)
-    draw_circ(sX, sY, sRad, "white", sWin)
-    draw_circ(sX, sY * 1.25, sRad * .82, "white", sWin)
+    draw_circ(sX, sY - sRad * 1.2, sRad * 1.21, "white", sWin)     #bottonm
+    draw_circ(sX, sY, sRad, "white", sWin)                  #middle
+    draw_circ(sX, sY + sRad * 1.2, sRad * .82, "white", sWin)     #top
     
-    draw_circ(sX - sY / 3, 180, sRad * .11, "black", sWin)
-    draw_circ(sX + sY / 3, 180, sRad * .11, "black", sWin)
+    draw_circ(sX - sRad / 4, sY + sRad * 1.5 , sRad * .11, "black", sWin)  #left eye 
+    draw_circ(sX + sRad / 4, sY + sRad * 1.5 , sRad * .11, "black", sWin)  #right eye
     
+##    nose = Polygon(Point(sX, sY+ sRad), Point(sX * 1.05, sY + sRad * 1.25), Point(sX * 1.2, sY + sRad*.71))
+##    nose.setOutline("orange")
+##    nose.setFill("orange")
+##    nose.draw(sWin)
+
 
 
 WinterWin = GraphWin("Winter Wonderland",600,600)
 WinterWin.setCoords(0,0,600,600)
 WinterWin.setBackground("light blue")
 
-anchX = random.randint(1,600)       
+draw_rect(0, 0, 600, 80, "light gray", WinterWin)
+
+anchX = 100      
 anchY = 140
 anchR = 28
 
-#bottom cirlce
-draw_circ(anchX, anchY * .71, anchR * 1.21, "white", WinterWin)
+for i in range (4):
+    draw_sm(anchX, anchY, anchR, "white",WinterWin)
 
-#middle circle
-draw_circ(anchX, anchY, anchR, "white", WinterWin)
-
-#top circle
-draw_circ(anchX, anchY * 1.25, anchR * .82, "white", WinterWin)
-
-#snowman eyes
-draw_circ(anchX - anchR / 3, 180, anchR * .11, "black", WinterWin)
-
-#----------------
-draw_circ(anchX + anchR / 3, 180, anchR * .11, "black", WinterWin)
-
-#nose
-nose = Polygon(Point(100,170), Point(105,175), Point(120,160))
-nose.setOutline("orange")
-nose.setFill("orange")
-nose.draw(WinterWin)
-
-#for i in range (4):
- 
-
-#Bottom snow 
-Rect = Rectangle(Point(0,0),Point (600,78))
-Rect.setFill("light gray")
-Rect.draw(WinterWin)
+##-------- Snow code--------
+###bottom cirlce
+##draw_circ(anchX, anchY * .71, anchR * 1.21, "white", WinterWin)
+##
+###middle circle
+##draw_circ(anchX, anchY, anchR, "white", WinterWin)
+##
+###top circle
+##draw_circ(anchX, anchY * 1.25, anchR * .82, "white", WinterWin)
+##
+###snowman eyes
+##draw_circ(anchX - anchR / 3, 180, anchR * .11, "black", WinterWin)
+##
+###----------------
+##draw_circ(anchX + anchR / 3, 180, anchR * .11, "black", WinterWin)
+##
+###nose
